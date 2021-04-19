e127b5efed3352839fe0525f2684c0869f8254d0
ruthwy <sairuthwikreddi@gmail.com>
2021-04-19 10:29:45 +0530

    database connection

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
+<?php 
 
+$host="localhost";
+$user="root";
+$password="";
+$db="ams";
+
+$con=  mysqli_connect($host,$user,$password);
+mysqli_select_db($con,$db);
+
+if(isset($_POST['username'])){
+    
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql="select * from login where username='".$uname."'AND password='".$password."' limit 1";
+    
+    $result=mysqli_query($con,$sql);
+    
+    if(mysqli_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }
+        
+}
+?>
 
 <html lang="en">
   <head>
