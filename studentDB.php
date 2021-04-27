1070e2ecc60e04c5a6909a58ff8b398d456bc05d
ruthwy <sairuthwikreddi@gmail.com>
2021-04-27 21:15:59 +0530

    done studentDB

--- studentDB.php
+++ studentDB.php
@@ -1,19 +1,19 @@
+<?php 
+session_start();
+
+
+if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
+
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>AMS Student Dashboard</title>
-        <link rel="stylesheet" type="text/css" href="./CSS/styles.css">
+        <link rel="stylesheet" type="text/css" href="./CSS/styles3.css">
         <style>
             .studentdb{
                 background: url(img/pic2.jpeg);
-            }
-            iframe{
-                top: 100%;
-    left: 47%;
-
-            }
-            label{
-                color:#fb2525;
             }
         </style>
     </head>
@@ -30,19 +30,84 @@
     </header>
     <body class="studentdb">
         <div class="label1">
-        <label for="Subject" ><b>CHOOSE A SUBJECT:</b></label>
+            <form method="POST">
+                <label for="Section"><b>CHOOSE YOUR SECTION:</label><br>
 
-        <select name="Subject" id="Subject">
-            <option value="none">-Select-</option>
-            <option value="CN">Computer Networks</option>
-            <option value="CD">Compiler Design</option>
-            <option value="SE">Software Engineering</option>
-            <option value="SSK">Softskills</option>
-        </select>
-        <br>
-        <input type="submit" name="" value="Submit"></div><br><br>
+                    <select name="section" id="Section">
+                        <option value="none">-Select-</option>
+                        <option value="class1">CLASS-1</option>
+                        <option value="class2">CLASS-2</option>
+                        <option value="class3">CLASS-3</option>
+                        <option value="class4">CLASS-4</option>
+                    </select><br>
+
+                <label for="Subject" ><b>CHOOSE A SUBJECT:</b></label>
+
+                    <select name="subject" id="Subject">
+                    <option value="none">-Select-</option>
+                    <option value="maths">MATHS</option>
+                    <option value="english">ENGLISH</option>
+                    <option value="science">SCIENCE</option>
+                    <option value="social">SOCIAL STUDIES</option>
+                    </select><br>
+
+                <input type="submit" name="" value="Submit"><br><br>
+            </form>
+        </div>
         
-        <iframe src="viewattendance.html" height="200" width="800" style="border:1px solid black" class="mx-5 my-5"></iframe>
+        <table>
+        <thead>
+            <tr>
+                <th>Date</th>
+                <th>status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $host="localhost";
+                $user="root";
+                $password="";
+                $db="ams";
+
+                $con=  mysqli_connect($host,$user,$password);
+                mysqli_select_db($con,$db);
+               
+                if(isset($_POST['section']) && isset($_POST['subject'])  ){
+                   
+                    $uname=$_SESSION['username'];
+                    $section=$_POST['section'];
+                    $subject=$_POST['subject'];
+                    
+                    $sql="SELECT date,status FROM `srcstud` WHERE username='$uname' AND section='$section' AND subject='$subject' ";
+                    
+                    $result=mysqli_query($con,$sql);
+
+                            while($res= mysqli_fetch_assoc($result))
+                            {
+                ?>
+                            <tr>
+                                <td><?php echo $res['date']; ?></td>
+                                <td><?php echo $res['status']; ?></td>
+                            <tr>
+                        <?php
+                        }
+                        ?>
+                <?php
+                }
+                ?>
+                <tr>
+                                <td>attendance percentage</td>
+                                <td>percentage</td>
+                            <tr>
+                    </tbody>
+                </table>
+    
     </body>
     </body>
 </html>
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
